636aadd2d6b4ad309a4c2002ec1a992dcf43c900
j-lambs <jacklol87@yahoo.com>
2024-06-30 00:07:02 -0700

    Fin C1, added documentation for FieldElement class

--- FieldElement.py
+++ FieldElement.py
@@ -1,4 +1,15 @@
 class FieldElement:
+    """
+    This class is a class for Field Elements, or elements of a Finite Field / Galois Field.
+    Functions include addition, subtraction, multipication, division, all of which are in line
+    with the properties of a finite field which are...
+    1. Elements being closed under addition and subtraction.
+    2. 0 existing and being the additive identity.
+    3. 1 existing and being the multiplicative identity.
+    4. There being an additive inverse.
+    5. There being a multiplicative inverse.
+    """
+
     def __init__(self, num, prime):
         if num >= prime or num < 0:
             error = 'Num {} not in field range 0 to {}'.format(num, prime - 1)
@@ -44,5 +55,12 @@ class FieldElement:
     def __truediv__(self, other):
         if self.prime != other.prime:
             raise TypeError('Cannot divide two numbers in different Fields')
-        num = 
+        # Taking advantage of Fermat's Little Theorem to turn division -> multiplication
+        num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
+        return self.__class__(num, self.prime)
+    
+    def __pow__(self, exponent):
+        n = exponent % (self.prime - 1)
+        num = pow(self.num, n, self.prime)
+        return self.__class__(num, self.prime)
         
